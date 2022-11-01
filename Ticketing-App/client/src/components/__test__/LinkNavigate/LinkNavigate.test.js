ec8ff23dde1f67da8326a82676e62fd2b0756bb1
123kavi <83937185+123kavi@users.noreply.github.com>
2022-11-01 19:57:47 +0530

    Unit testing 2

---
+++ Ticketing-App/client/src/components/__test__/LinkNavigate/LinkNavigate.test.js
@@ -0,0 +1,19 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import "@testing-library/jest-dom";
+import LinkNavigate from "../../LinkNavigate/LinkNavigate";
+import React from 'react';
+afterEach(() => {
+  cleanup();
+});
+
+test("renders correctly", () => {
+  const component = renderer.create(
+    <LinkNavigate page="https://github.com/aseemwangoo">Made with ❤️ - Aseem</LinkNavigate>
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+
