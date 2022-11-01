e1ef76475bbddf6109e158fb6951c6b2301e1307
123kavi <83937185+123kavi@users.noreply.github.com>
2022-11-01 20:26:29 +0530

    Unittesting

---
+++ Ticketing-App/client/src/components/nmnm/w.js
@@ -0,0 +1,22 @@
+// import{render,screen,cleanup} from '@testing-library/react'
+// import DashboardLogin from '../admin-components/DashboardLogin';
+// import React from 'react';
+
+
+
+
+
+// test('Should Render Login Components',()=>{
+
+// render(<DashboardLogin/>);
+// const todoElement =screen.getByTestId('todo-2');
+
+// expect (todoElement).toBeInTheDocument();
+// expect(todoElement).toHaveTextContent('LOGIN');
+
+
+
+
+
+
+// })
