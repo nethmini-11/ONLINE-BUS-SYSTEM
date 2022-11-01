839cc01e5183e9fb672fc397df9f114ad232c6b8
Nethmini__0_11 <manthikaoriginal@gmail.com>
2022-11-01 22:16:17 +0530

    unit test for add account component

---
+++ Ticketing-App/client/src/components/__test__/test-m/AddAccount.test.js
@@ -0,0 +1,23 @@
+import{render,screen,cleanup} from '@testing-library/react'
+import AddAccount from '../../admin-components/AddAccount';
+import React from 'react';
+import "@testing-library/jest-dom";
+
+
+
+
+
+test('Render add account.jsx',()=>{
+
+render(<AddAccount/>);
+const todoElement =screen.getByTestId('add-account');
+
+expect (todoElement).toBeInTheDocument();
+expect(todoElement).toHaveTextContent('Create User Account');
+
+
+
+
+
+
+})
