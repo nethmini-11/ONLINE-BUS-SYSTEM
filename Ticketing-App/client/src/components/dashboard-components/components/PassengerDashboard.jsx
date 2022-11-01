7f61c6b2e38b1e3d35d6b5867a7217e2b7a5fa47
nethmini-11 <83303587+nethmini-11@users.noreply.github.com>
2022-11-01 13:07:32 +0530

    Update PassengerDashboard.jsx

--- Ticketing-App/client/src/components/dashboard-components/components/PassengerDashboard.jsx
+++ Ticketing-App/client/src/components/dashboard-components/components/PassengerDashboard.jsx
@@ -141,76 +141,10 @@ class PassengerDashboard extends Component {
                                         </div>
                                     </div>
                                 </div>
-                                {/* <div className="col-md-8">
-                                    <div className="single-report mb-xs-30">
-                                        <div className="s-report-inner pr--20 pt--30 mb-3">
-                                            <div className="icon">
-                                                <i className="ti-map-alt"/>
-                                            </div>
-                                            <div className="s-report-title d-flex justify-content-between">
-                                                <h4 className="header-title mb-0">
-                                                    Last Visited Place
-                                                </h4>
-                                            </div>
-                                            <div className="d-flex justify-content-between pb-2">
-                                                
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> */}
-
-
-<br></br>
-
-                                {/* <div className="col-md-4">
-                                <div className="single-report mb-xs-30">
-                                    <div className="s-report-inner pr--20 pt--30 mb-3" onClick={() => {
-                                        this.qrCodeAction()
-                                    }}>
-                                        <div className="icon">
-                                            <i className="ti-harddrives"/>
-                                        </div>
-                                        <div className="s-report-title d-flex justify-content-between">
-                                            <h4 className="header-title mb-0">Short Term QR Service</h4>
-                                        </div>
-                                        
-                                    </div>
-                                </div>
-                            </div> */}
-
-
-
-                            {/* <div className="col-md-4">
-                                <div className="single-report mb-xs-30">
-                                    <div className="s-report-inner pr--20 pt--30 mb-3" onClick={() => {
-                                        this.qrCodeAction2()
-                                    }}>
-                                        <div className="icon">
-                                            <i className="ti-harddrives"/>
-                                        </div>
-                                        <div className="s-report-title d-flex justify-content-between">
-                                            <h4 className="header-title mb-0">Long term QR Service</h4>
-                                        </div>
-                                        
-                                    </div>
-                                </div>
-                            </div> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                              
                             </div>
                         </div>
+                        <div className="row" style={{marginBottom:100}}><img src="assets/images/icon/cover.png" style={{height:250 , width:1200}} alt="login" /></div>
                         {/* sales report area end */}
                         {/* overview area start */}
                         <div className="row">
